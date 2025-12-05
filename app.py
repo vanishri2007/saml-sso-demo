7d129a7dcc6098c47f93a8164fc8ff28fb1a719a
vanishri2007 <vanishri2007@gmail.com>
2025-12-04 23:29:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,5 @@ def metadata():
 
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
